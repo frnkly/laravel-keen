26ddb05d2a9e1d0bf9368ccf88254a43e32ce3eb
Frank <frank@frnk.ca>
2017-10-09 17:15:24 -0400

    Using laravel config

--- src/TracksRequests.php
+++ src/TracksRequests.php
@@ -34,46 +34,50 @@ class TrackRequests
     {
         $response = $next($request);
 
-        // Gather defaults to track every request
-        $path = substr($request->path(), strpos($request->path(), '/'));
-        $params = $request->toArray();
-        $fingerprint = null;
-
-        // Try to retrieve route information
-        if ($request->route()) {
-            $params += $request->route()->parameters();
-            $fingerprint = $request->fingerprint();
+        if (! config('services.keen.track-requests', true)) {
+            return $response;
         }
 
-        $this->client->addEvent('request', [
+        // Build event data
+        $eventData = [
             'method'        => $request->method(),
             'host'          => $request->root(),
-            'path'          => $path,
-            'params'        => $params,
-            'fingerprint'   => $fingerprint,
+            'path'          => substr($request->path(), strpos($request->path(), '/')),
+            'params'        => $request->toArray(),
+            'fingerprint'   => '',
             'ip'            => $request->ip(),
             'user_agent'    => $request->headers->get('user-agent'),
             'response'      => [
                 'time' => microtime(true) - $this->startTime,
                 'code' => $response->getStatusCode(),
             ],
+        ];
 
-            // Keen add-ons
-            'keen' => [
-                'addons' => [
-                    [
-                        'name'   => 'keen:ip_to_geo',
-                        'output' => 'geo_data',
-                        'input'  => ['ip' => 'ip'],
-                    ],
-                    [
-                        'name'   => 'keen:ua_parser',
-                        'output' => 'user_data',
-                        'input'  => ['ua_string' => 'user_agent'],
-                    ]
-                ]
-            ]
-        ]);
+        // Try to retrieve route information
+        if ($request->route()) {
+            $eventData['params'] += $request->route()->parameters();
+            $eventData['fingerprint'] = $request->fingerprint();
+        }
+
+        // Add geo-location data
+        if (config('services.keen.geo-data', true)) {
+            $eventData['keen']['addons'][] = [
+                'name'  => 'keen:ip_to_geo',
+                'output'=> 'geo_data',
+                'input' => ['ip' => 'ip']
+            ];
+        }
+
+        // Add user-agent data
+        if (config('services.keen.user-data', true)) {
+            $eventData['keen']['addons'][] = [
+                'name'   => 'keen:ua_parser',
+                'output' => 'user_data',
+                'input'  => ['ua_string' => 'user_agent']
+            ];
+        }
+
+        $this->client->addEvent('request', $eventData);
 
         return $response;
     }
